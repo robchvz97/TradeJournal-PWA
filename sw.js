6909a99c026e9aacfd9f6af3f869b7231a28ddeb
robchvz97 <46147703+robchvz97@users.noreply.github.com>
2026-09-24 23:16:31 -0600

    Bump cache to v3 for refresh button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tradejournal-v2';
+const CACHE = 'tradejournal-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
